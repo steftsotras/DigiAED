3dc13f3e94f38f3f8b1c9be7b41869ec4a9edea5
Stefanos Tsotras <steftsotras2@gmail.com>
2020-01-05 05:07:11 +0200

    report ready

--- app/src/main/java/com/example/digiaed/ReportActivity.java
+++ app/src/main/java/com/example/digiaed/ReportActivity.java
@@ -1,5 +1,6 @@
 package com.example.digiaed;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,9 +9,18 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ReportActivity extends AppCompatActivity {
 
@@ -20,14 +30,19 @@ public class ReportActivity extends AppCompatActivity {
     private RadioButton radioButton;
     private EditText txt_comment;
     private Button btn_report;
+    private ProgressBar progressBar9;
 
     private Intent intent;
     private String marker_id;
+
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+
+        db = FirebaseFirestore.getInstance();
 
         intent = getIntent();
         marker_id = intent.getStringExtra("Id");
@@ -35,10 +50,13 @@ public class ReportActivity extends AppCompatActivity {
         radioGroup = (RadioGroup) findViewById(R.id.radio);
         txt_comment = (EditText) findViewById(R.id.textComment);
         btn_report = (Button) findViewById(R.id.btnReport2);
+        progressBar9 = (ProgressBar) findViewById(R.id.progressBar9);
 
         btn_report.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                progressBar9.setVisibility(View.VISIBLE);
 
                 int radioChecked = radioGroup.getCheckedRadioButtonId();
                 radioButton = findViewById(radioChecked);
@@ -48,7 +66,30 @@ public class ReportActivity extends AppCompatActivity {
 
                 Log.d(TAG,"marker: "+marker_id+"coment: "+com+" choice: "+choice);
 
-                
+
+                Map<String, Object> aedreport = new HashMap<>();
+                aedreport.put("AEDMapID",marker_id);
+                aedreport.put("Comment",com);
+                aedreport.put("Type",choice);
+
+
+                db.collection("AEDReport").add(aedreport).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+
+                        progressBar9.setVisibility(View.GONE);
+                        startActivity(new Intent(ReportActivity.this, AEDMapActivity.class));
+                    }
+                })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                                progressBar9.setVisibility(View.GONE);
+                            }
+                        });
+
             }
         });
     }
